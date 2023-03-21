8c58952e5c9e4d32d15da32e0cbe41d515e1c5e8
hongxeob <sjun2918@naver.com>
2023-03-21 14:22:36 +0900

    add : @ComponentScan의 기능

---
+++ core/src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -0,0 +1,27 @@
+package hello.core.scan;
+
+import hello.core.AutoAppConfig;
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AutoAppConfigTest {
+
+    @Test
+    void basicScan() throws Exception {
+        //given
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+        //when
+        MemberService memberService = ac.getBean(MemberService.class);
+
+        //then
+        assertThat(memberService).isInstanceOf(MemberService.class);
+
+    }
+
+}
