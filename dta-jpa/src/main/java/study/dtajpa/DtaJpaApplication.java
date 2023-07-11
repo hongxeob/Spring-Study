7a1a4b7bb349a2d45cbdfadf95a8fd2ad37f391a
Hongseob Lee <sjun2918@naver.com>
2023-07-11 15:00:11 +0900

    chore : Spring Data Jpa 세팅

---
+++ dta-jpa/src/main/java/study/dtajpa/DtaJpaApplication.java
@@ -0,0 +1,13 @@
+package study.dtajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtaJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DtaJpaApplication.class, args);
+    }
+
+}
---
+++ dta-jpa/src/test/java/study/dtajpa/DtaJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package study.dtajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtaJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
